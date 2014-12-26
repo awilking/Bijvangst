0a6d6adcf479ec2e0f9b4ba2bccae26cab24adbd
Andre Wilkinghoff <andre@wilkinghoff.de>
2014-12-26 16:50:23 +0100

    initial

---
+++ inc/version.h
@@ -0,0 +1,9 @@
+
+#ifndef INC_VERSION_H
+#define INC_VERSION_H
+
+#define VER_MAJOR 0
+#define VER_MINOR 0
+#define VER_MICRO 1
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,27 @@
+/**
+\mainpage "Bijvangst
+
+	#Purpose#:
+	$Workfile: main.c $
+	$Revision:	$
+	$Date:	$
+
+\internal Under construction
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/** 
+	main entry.
+*/
+int main( int nArg, char **pArgv)
+{
+	int nRetCode = 0;
+
+	return( nRetCode);
+}
+
+
+
